b77cafdd9470049ff8a005d8e2bc530b729bd9aa
Keisuke Yasuda <keisuke0319@gmail.com>
2018-11-14 02:25:19 +0900

    AssetRequest.dll削除。自作AssetRequest追加（名前は変える予定）。 README日記更新。

---
+++ Assets/Editor/AssetRequest/AssetRequest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ImportAssets : EditorWindow
+{
+    [MenuItem("Window/AssetsRequest/Import assets from the list")]
+    static void Open()
+    {
+        GetWindow<ImportAssets>();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Import Assets");
+    }
+}
+
+public class MakeList : EditorWindow
+{
+    [MenuItem("Window/AssetsRequest/Make request list")]
+    static void Open()
+    {
+        GetWindow<MakeList>();
+    }
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Make the request list");
+    }
+
+}
